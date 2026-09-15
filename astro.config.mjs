ae6c266f6a2d15e1f00076259b536e7098c7c58f
史蒂夫 <lizhichao0820@qq.com>
2026-09-15 09:45:11 +0800

    chore: site 切换为正式域名 meiriyangmao.top

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,6 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://daily-yangmao.2315935618.workers.dev',
+  site: 'https://meiriyangmao.top',
   integrations: [sitemap()],
 });
